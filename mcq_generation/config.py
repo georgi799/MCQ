7c823b4d330ad2badb50bd2efa2a71c1b0d0a8cb
georgi799 <nicoleta.rus@student.upt.ro>
2025-06-26 17:36:07 +0300

    Connecting database
    Changes made to rag.py, mcq_gen.py and added FastAPI endpoint. Loading environment variables instead of hardcoded credentials

---
+++ mcq_generation/config.py
@@ -0,0 +1,16 @@
+from transformers import AutoTokenizer, AutoModel
+from keybert import KeyBERT
+
+MODEL_NAME = "intfloat/multilingual-e5-large"
+
+from langchain_huggingface import HuggingFaceEmbeddings
+embedding_model = HuggingFaceEmbeddings(
+    model_name=MODEL_NAME,
+    encode_kwargs={"normalize_embeddings": True}
+)
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModel.from_pretrained(MODEL_NAME)
+model.eval()
+
+kw_model = KeyBERT(model=model)
